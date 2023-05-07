7af988d0fdf4dd217b73b50ed88b3821bcb5e8b1
suneesh <suneeshnkd007@gmail.com>
2023-05-07 21:24:11 +0530

    sucess

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active-nav');
 }
+
+function sentmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "suneeshs793@gmail.com",
+        Password : "suneesh2003",
+        To : 'suneeshs793@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("sub").value,
+        Body : "Name : " + document.getElementById("name").value + "<br>" + 
+               "Number : " + document.getElementById("email").value + "<br>" + 
+               "Message : " + document.getElementById("msg").value 
+    }).then(
+      message => alert("email sent")
+    );
+}
