10c60301d7a6319ba88ae223a3608273f4298b9b
suneesh <suneeshnkd007@gmail.com>
2023-05-09 13:32:46 +0530

    commited

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.nav-bar');
+let btn_home = document.querySelector('#hme');
+let btn_about = document.querySelector('#abt');
+let btn_project = document.querySelector('#projet');
+let btn_contact = document.querySelector('#cotact');
 
+
+btn_home.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active-nav');
+    btn_home.classList.add('active');
+    btn_about.classList.remove('active');
+    btn_project.classList.remove('active');
+    btn_contact.classList.remove('active');
+}
+btn_about.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active-nav');
+    btn_about.classList.add('active');
+    btn_home.classList.remove('active');
+    btn_project.classList.remove('active');
+    btn_contact.classList.remove('active');
+}
+btn_project.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active-nav');
+    btn_project.classList.add('active');
+    btn_home.classList.remove('active');
+    btn_about.classList.remove('active');
+    btn_contact.classList.remove('active');
+
+}
+btn_contact.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active-nav');
+    btn_contact.classList.add('active');
+    btn_home.classList.remove('active');
+    btn_about.classList.remove('active');
+    btn_project.classList.remove('active');
+
+}
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active-nav');
